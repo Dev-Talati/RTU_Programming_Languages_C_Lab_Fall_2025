ccbb9d16dd8fe960aa3abf026ea2a8a5d6857c6f
Dev-Talati <dev-rakeshbhai.talati@edu.rtu.lv>
2025-10-07 10:24:19 +0000

    Lab4 Completed

--- src/week4_3_struct_database.c
+++ src/week4_3_struct_database.c
@@ -1,7 +1,7 @@
 /*
  * week4_3_struct_database.c
- * Author: [Your Name]
- * Student ID: [Your ID]
+ * Author: [Dev Talati]
+ * Student ID: [241ADB037]
  * Description:
  *   Simple in-memory "database" using an array of structs.
  *   Students will use malloc to allocate space for multiple Student records,
@@ -12,27 +12,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-// TODO: Define struct Student with fields name, id, grade
+struct Student {
+  char name[50];
+  int id;
+  float grade;
+};
 
 int main(void) {
-    int n;
-    struct Student *students = NULL;
+  int n;
+  struct Student *students = NULL;
 
-    printf("Enter number of students: ");
-    if (scanf("%d", &n) != 1 || n <= 0) {
-        printf("Invalid number.\n");
-        return 1;
+  printf("Enter number of students: ");
+  if (scanf("%d", &n) != 1 || n <= 0) {
+    printf("Invalid number.\n");
+    return 1;
+  }
+
+  students = (struct Student *)malloc(n * sizeof(struct Student));
+  if (students == NULL) {
+    printf("Memory allocation failed.\n");
+    return 1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    printf("Enter data for student %d: ", i + 1);
+    // Read name (no spaces), id, and grade
+    if (scanf("%49s %d %f", students[i].name, &students[i].id,
+              &students[i].grade) != 3) {
+      printf("Invalid input.\n");
+      free(students);
+      return 1;
     }
+  }
 
-    // TODO: Allocate memory for n Student structs using malloc
+  printf("\n%-7s %-12s %-5s\n", "ID", "Name", "Grade");
+  for (int i = 0; i < n; i++) {
+    printf("%-7d %-12s %.1f\n", students[i].id, students[i].name,
+           students[i].grade);
+  }
 
-    // TODO: Read student data in a loop
-
-    // TODO: Display all student records in formatted output
-
-    // Optional: Compute average grade or find top student
-
-    // TODO: Free allocated memory
-
-    return 0;
+  free(students);
+  return 0;
 }
